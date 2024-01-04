91912250ccd51fe913983b90466f0fb75c8d5bb5
jbuilds <jakemacri561@gmail.com>
2024-01-04 16:14:04 -0500

    push

--- events_query.py
+++ events_query.py
@@ -3,29 +3,35 @@ import json
 
 
 def events_search():
-    '''
+    """
     command reference:
     delegator reward
-    seid query txs --events 'message.sender=cosmos1...&message.action=withdraw_delegator_reward' 
+    seid query txs --events 'message.sender=cosmos1...&message.action=withdraw_delegator_reward'
     --page 1 --limit 30 --chain-id pacific-1 --node https://sei-rpc.brocha.in/ --output json
 
     contract creations
-    seid query txs --events 'message.sender=cosmos1...&message.action=withdraw_delegator_reward' 
+    seid query txs --events 'message.sender=cosmos1...&message.action=withdraw_delegator_reward'
     --page 1 --limit 30 --chain-id pacific-1 --node https://sei-rpc.brocha.in/ --output json
-    '''
+    """
 
     command = [
         "seid",
         "query",
         "txs",
         "--events",
-        "wasm.register_collection=success",
-        "--height", "49717431",
-        "--page", "1",
-        "--limit", "1",
-        "--chain-id", "pacific-1",
-        "--node", "https://sei-rpc.brocha.in/",
-        "--output", "json",
+        "wasm.action=create_pair",
+        "--height",
+        "49722499",
+        "--page",
+        "1",
+        "--limit",
+        "3",
+        "--chain-id",
+        "pacific-1",
+        "--node",
+        "https://sei-rpc.brocha.in/",
+        "--output",
+        "json",
     ]
     result = subprocess.run(command, capture_output=True, text=True)
     if result.stdout:
@@ -43,8 +49,12 @@ if __name__ == "__main__":
         print(f"count: {output['count']}")
         print(f"page total: {output['page_total']}")
         print(f"Transactions: {len(output['txs'])}")
+        print(f"Hashes:")
+        for tx in output["txs"]:
+            print(tx["txhash"])
+            print(tx["height"])
         print()
-        print(json.dumps(output, indent=2))
+        # print(json.dumps(output, indent=2))
         print()
     else:
         print("Events Search is None...")
